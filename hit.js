8752f7eb95356a4de9460cc51c68f05b1f3586b1
vikashthakur34348 <vikashthakur3438@gmail.com>
2026-06-01 20:18:52 +0530

    Add HTML and JS files

---
+++ hit.js
@@ -0,0 +1,15 @@
+// for(let i=1;i<=100;i++){
+//     console.log("vikash");
+// }
+
+//while
+// let i=1;
+// while(i<=5){
+// console.log(i);
+//}
+
+//do while
+let q=0;
+do{
+    console.log("vikash");
+}while(q<=9); 
---
+++ html.js
@@ -0,0 +1,78 @@
+// // let marks=[12,34,56,78,9,90];
+// // console.log(marks);
+// // let name=["vikash","rohit","amit"];
+// // console.log(name);
+
+// // let arry=["vikash","rohit","amit","sonu","vishal"];
+// // for(let i=0;i<arry.length;i++){
+// //     console.log(arry[i]);
+// // }
+ 
+// let arr=[85,97,44,37,76,60];
+// let sum=0;
+// let avg=0;
+// for(let i of arr){
+//     console.log(i);
+//     sum=sum+i;
+//     avg=sum/length.i;
+
+//     // console.log(sum)
+// }
+// console.log(sum);
+// console.log(`the avg of arr ${avg}`);
+
+//partice set
+let pen = [250, 645, 300, 900, 50];
+// let i = 0;
+
+// for (let val of pen) {
+//     console.log(val);
+//    let off = val / 10;
+//     pen[i] = pen[i] - off;
+//     console.log(pen[i]);
+//      i++
+// }
+
+// for(let i=0;i<pen.length;i++){
+//     let offer=pen[i]/10;
+//     pen[i]-=offer;
+
+// }
+// console.log(pen);
+
+// //
+// let iteams=["apple","manogo","litchi","orange"]
+// console.log(iteams);
+// // let additeams=iteams.push("vikash");
+// // console.log("add",additeams);
+// // console.log(iteams);
+// let deleteiteams=iteams.pop(iteams);
+// console.log("delete",deleteiteams);
+// console.log(iteams);
+
+// /// array conact
+// let hero=["ktjg","jhgcliu","t5hgotgi","tugb4oti"];
+// let hero1=["thoc","tuyg","thgfno"];
+
+// let herio=hero.concat(hero1);
+// console.log(herio);
+
+// // unshift
+// let car=["alto","nano","truck"];
+// let newdata=car.unshift("bike");
+// console.log(newdata);
+
+// //slice arry
+// let car=["alto","nano","truck","vikash","idjg"];
+// console.log(car);
+// console.log(car.slice(1,4));
+
+
+// splice
+// let a=[12,34,56,789,90,4];
+// console.log(a);
+// a.splice(2,1,23334);
+
+// partice
+let store=["Bloombarg","microsoft","uber","google","ibm","netflex"];
+console.log(store);
